d3042523d332f77fe11a2c2f2752c936ec6a7c12
segfault0x61 <torresj1997@outlook.com>
2023-02-10 12:47:33 -0600

    refactor: add better solution for Two Sum problem

--- src/001-two-sum.ts
+++ src/001-two-sum.ts
@@ -1,10 +1,20 @@
+//-----------------------------------------------------------------------------
+// Runtime: 62ms
+// Memory Usage: 46.1 MB
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// Link: https://leetcode.com/submissions/detail/895519673/
+//-----------------------------------------------------------------------------
+
 export function twoSum(nums: number[], target: number): number[] {
+  const map = new Map<number, number>();
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement) as number, i];
     }
+    map.set(nums[i], i);
   }
+
   return [];
 }
