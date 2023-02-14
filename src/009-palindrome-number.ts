501769983e774bc109b82f9f80dc24ca25646333
segfault0x61 <torresj1997@outlook.com>
2023-02-14 14:30:32 -0600

    refactor: add notes to LC #9

--- src/009-palindrome-number.ts
+++ src/009-palindrome-number.ts
@@ -6,6 +6,15 @@
 // Link: https://leetcode.com/submissions/detail/898062312/
 //-----------------------------------------------------------------------------
 
+
+
+/*
+The idea is to use one pointer to traverse the integer from the left side and another 
+pointer to traverse the integer from the right side. We compare the digits at the two 
+pointers in each iteration and return false if they are not equal. If we reach the middle 
+of the integer without finding any unequal digits, we return true. 
+*/
+
 export function isPalindrome(x: number): boolean {
   if (x < 0) return false;
 
